18d21d8072eeec0a98454b8174f52e6cce3f02ed
Rodrigo Branas <rodrigo.branas@gmail.com>
2021-05-18 11:33:50 -0300

    updated

---
+++ validateCpf_after.js
@@ -0,0 +1,49 @@
+const FACTOR_DIGIT_1 = 10;
+const FACTOR_DIGIT_2 = 11;
+const MAX_DIGITS_1 = 9;
+const MAX_DIGITS_2 = 10;
+
+function validateCpf(cpf = "") {
+	cpf = extractDigits(cpf);
+	if (isInvalidLength(cpf)) return false;
+	if (isBlocked(cpf)) return false;
+	const digit1 = calculateDigit(cpf, FACTOR_DIGIT_1, MAX_DIGITS_1);
+	const digit2 = calculateDigit(cpf, FACTOR_DIGIT_2, MAX_DIGITS_2);
+	let calculatedCheckDigit = `${digit1}${digit2}`;  
+	return getCheckDigit(cpf) == calculatedCheckDigit;
+}
+
+function extractDigits(cpf) {
+	return cpf.replace(/\D/g, "");
+}
+
+function isInvalidLength(cpf) {
+	return cpf.length !== 11;
+}
+
+function isBlocked(cpf) {
+	const [digit1] = cpf;
+	return cpf.split("").every(digit => digit === digit1);
+}
+
+function calculateDigit(cpf, factor, max) {
+	let total = 0;
+	for (const digit of toDigitArray(cpf).slice(0, max)) {
+		total += digit * factor--;
+	}
+	return (total%11 < 2) ? 0 : (11 - total%11);
+}
+
+function toDigitArray(cpf) {
+	return [...cpf].map(digit => parseInt(digit));
+}
+
+function getCheckDigit(cpf) {
+	return cpf.slice(9);
+}
+
+console.log(validateCpf("00000000000"));
+console.log(validateCpf("86446422799"));
+console.log(validateCpf("86446422784"));
+console.log(validateCpf("864.464.227-84"));
+console.log(validateCpf("91720489726"));
---
+++ validateCpf_before.js
@@ -0,0 +1,51 @@
+function validate(str) {
+	if (str !== null && str !== undefined) {
+		if (str.length >= 11 || str.length <= 14){  
+			str=str.replace('.','');  
+			str=str.replace('.','');  
+			str=str.replace('-','');  
+			str=str.replace(" ","");  
+			if (!str.split("").every(c => c === str[0])) {
+				try{  
+					let     d1, d2;  
+					let     digito1, digito2, resto;  
+					let     digito;  
+					let     nDigResult;  
+					d1 = d2 = 0;  
+					digito1 = digito2 = resto = 0;  
+						
+					for (let nCount = 1; nCount < str.length -1; nCount++) {  
+						digito = parseInt(str.substring(nCount -1, nCount));  
+						d1 = d1 + ( 11 - nCount ) * digito;  
+						d2 = d2 + ( 12 - nCount ) * digito;  
+					};  
+						
+					resto = (d1 % 11);  
+		
+					digito1 = (resto < 2) ? digito1 = 0 : 11 - resto;  
+						
+					d2 += 2 * digito1;  
+		
+					resto = (d2 % 11);  
+		
+					if (resto < 2)  
+						digito2 = 0;  
+					else  
+						digito2 = 11 - resto;  
+						
+					let nDigVerific = str.substring(str.length-2, str.length);  
+					nDigResult = "" + digito1 + "" + digito2;  
+					return nDigVerific == nDigResult;
+				}catch (e){  
+					console.error("Erro !"+e);  
+					return false;  
+				}  
+			} else return false
+		}else return false;
+	} else return false;
+}
+
+console.log(validate("00000000000"));
+console.log(validate("86446422784"));
+console.log(validate("864.464.227-84"));
+console.log(validateCpf("91720489726"));
